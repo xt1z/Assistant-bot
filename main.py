3a669ba14204d1633f662e1749d6a42c5f4b83fd
Chutipon <96512334+xt1z@users.noreply.github.com>
2024-03-15 17:45:36 +0700

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import ffmpeg
 import json
 from discord.ext import commands
 from discord import Activity, ActivityType
+from keep_alive import keep_alive
+keep_alive()
 # Retrieve bot token from environment variable
 TOKEN = os.environ.get('TOKEN')
 
